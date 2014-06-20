446ced7fc838a4f60a6819615988474d5916e26c
Tim Gleue <tim@gleue-interactive.com>
2014-06-20 13:34:07 +0200

    Renamed first table item to include double tap hint again

--- TGLStackedViewExample/TGLTableViewController.m
+++ TGLStackedViewExample/TGLTableViewController.m
@@ -27,22 +27,29 @@
 #pragma mark - Accessors
 
 - (NSArray *)segues {
+    
     if (_segues == nil) {
-        if(floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_6_1) {
-            _segues = @[@"Show stand-alone", @"Show in NavigationController", @"Show with Toolbar"];
-        }else {
-            _segues = @[@"Show stand-alone", @"Show in NavigationController", @"Show with Toolbar", @"Show without ExtendedEdges"];
+        
+        if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_6_1) {
+            
+            _segues = @[ @"Stand-alone (double tap to close)", @"Show in NavigationController", @"Show with Toolbar" ];
+            
+        } else {
+            
+            _segues = @[ @"Stand-alone (double tap to close)", @"Show in NavigationController", @"Show with Toolbar", @"Show without ExtendedEdges" ];
         }
     }
     
     return _segues;
 }
 
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
     return self.segues.count;
 }
 
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
     //obviously not efficient but we're not doing anything special.
     UITableViewCell* cell = [[UITableViewCell alloc] init];
     [cell.textLabel setText:self.segues[indexPath.row]];
@@ -50,13 +57,14 @@
     return cell;
 }
 
--(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+
     [self performSegueWithIdentifier:self.segues[indexPath.row] sender:nil];
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     
-    if ([segue.identifier isEqualToString:@"Show stand-alone"]) {
+    if ([segue.identifier isEqualToString:@"Stand-alone (double tap to close)"]) {
         
         TGLViewController *controller = segue.destinationViewController;
         
